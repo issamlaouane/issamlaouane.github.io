fef4cafe23dc4cff5159d349eec1ea8d51c62f9c
issL <136663150+issamlaouane@users.noreply.github.com>
2023-06-16 15:04:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("header").classList.add("active");
+  document.getElementById("experiences").classList.add("active");
+  document.getElementById("skills").classList.add("active");
+  document.getElementById("education").classList.add("active");
+});
